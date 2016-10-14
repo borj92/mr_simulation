36107513bb7d9c8a617fd6960251f8c8876924bb
borj92 <ben.jordan92@hotmail.co.uk>
2016-10-14 17:31:42 +0100

    A function to crop Amira datasets

---
+++ cropAmira.m
@@ -0,0 +1,17 @@
+%% Function to crop amira datasets, to just simulate on a small portion of
+%the network.
+%
+% Inputs:
+%       + full  -  Amira dataset to be cropped.
+%       + X     -  Two element vector of X range.
+%       + Y     -  Two element vector of Y range.
+%       + Z     -  Two element vector of Z range.
+%
+% Outputs:
+%       + cropped  -  Cropped amira dataset
+
+function [cropped] = cropAmira(full, X, Y, Z)
+
+
+
+end
